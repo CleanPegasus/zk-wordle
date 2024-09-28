ab93de053cc40dc4ae3184a55e12686d010af981
CleanPegasus <arunk609@gmail.com>
2024-09-28 14:02:01 +0530

    ✨ proof verification

--- scripts/play.js
+++ scripts/play.js
@@ -1,7 +1,12 @@
 const circomlibjs = require("circomlibjs");
 const snarkjs = require("snarkjs");
+const fs = require("fs");
 
 const readline = require("readline");
+
+function wordToLowercaseAscii(word) {
+  return word.toLowerCase().split('').map(char => char.charCodeAt(0));
+}
 
 function askQuestion(query) {
   const rl = readline.createInterface({
@@ -17,12 +22,59 @@ function askQuestion(query) {
   );
 }
 
+async function generateProof(attempts, answer) {
+  const { proof, publicSignals } = await snarkjs.groth16.fullProve(
+    { attempts: attempts, answer: answer }, 
+    "build/wordle_js/wordle.wasm", 
+    "helpers/wordle_0001.zkey");
+
+    return [proof, publicSignals]
+}
+
 async function main(answer) {
-  let attempt0 = await askQuestion("Attempt 1:");
-  let result = matchAttemptAnswer(attempt0, answer);
-  console.log(result);
-  let resultEmoji = result.map((res) => resultToEmoji(res));
-  console.log(resultEmoji);
+  let results = []
+  let resultsEmojis = []
+  let attempts = [];
+
+  for(var i=0; i<5; i++) {
+    let attempt = await askQuestion("Attempt ", i + 1);
+    attempts.push(wordToLowercaseAscii(attempt));
+    let result = matchAttemptAnswer(attempt, answer);
+    results.push(result);
+    let resultEmoji = result.map((res) => resultToEmoji(res));
+    resultsEmojis.push(resultEmoji);
+    console.log(resultsEmojis);
+  }
+  let answer_ascii = wordToLowercaseAscii(answer);
+  const [proof, publicSignals] = await generateProof(attempts, answer_ascii);
+
+  
+  console.log("proof: ", proof);
+  console.log("publicSignals: ", publicSignals);
+
+  const publicSignalsHash = publicSignals[0];
+
+
+  const res = await verifyProof({proof, publicSignals, results });
+  
+
+  console.log("res: ", res);
+
+
+}
+
+async function verifyProof({proof, publicSignals, results}) {
+  const vKey = JSON.parse(fs.readFileSync("verification_key.json"));
+  const res = await snarkjs.groth16.verify(vKey, publicSignals, proof);
+  
+  publicSignals.pop();
+
+  const resultFlat = results.flat(1);
+
+  console.log("resultFkat: ", resultFlat);
+  console.log("res: ", publicSignals);
+  
+  return res;
 }
 
 function matchAttemptAnswer(attempt, answer) {
@@ -53,4 +105,8 @@ function resultToEmoji(val) {
     return "\u{2B1B}";
   }
 }
-main("hello");
+
+
+main("hello").then(() => {
+  process.exit(0);
+})
